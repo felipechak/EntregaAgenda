5f8574b4d3ddc680e684cf31895c5bae1b92da21
felipechak <felipepacin7@gmail.com>
2023-08-29 15:49:47 -0300

    Corrected delete contact function and added edit menu

--- main.js
+++ main.js
@@ -19,20 +19,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const convertToHTMLElement = (id,name, phone) => {
         return `
                 <li class="list-group-item d-flex justify-content-between align-content-center">
-                <div class="d-flex flex-row">
-                    <img src="img/file-person-fill.svg" width="40" />
-                    <div class="ml-2">
-                        <h6 class="mb-0">${name}</h6>
-                        <div class="about">
-                            <span>${phone}</span>
+                    <div class="d-flex flex-row">
+                        <img src="img/file-person-fill.svg" width="40" />
+                        <div class="ml-2">
+                            <h6 class="mb-0">${name}</h6>
+                            <div class="about">
+                                <span>${phone}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="check">
-                <button onclick="eliminarContacto(${id})">
-                  <img src="img/trash-fill.svg""/>
-                </button>
-                </div>
+                    <div class="check">
+                        <button class="delete-button">
+                          <img src="img/trash-fill.svg""/>
+                        </button>
+                        <button class="edit-menu" type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">
+                          <img src="https://icons.getbootstrap.com/assets/icons/pencil-fill.svg"/>
+                        </button>
+                    </div>
                 </li>`
     }
 
@@ -55,15 +58,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
         lista.push(nuevoElemento)
         console.log(lista)
         window.localStorage.setItem("lista",JSON.stringify(lista))
+        refrescarLista()
     }
-
+    const actualizarContacto = (id,nuevoNumero) => {
+        let lista = leerLista()
+        for (let i = 0; i < lista.length; i++) {
+            if(lista[i].id == id){
+                lista[i].telefono = nuevoNumero;
+                break;
+            }
+        }
+        window.localStorage.setItem("lista",JSON.stringify(lista))
+        refrescarLista()
+    }
     function eliminarContacto(id) {
         console.log('borro')
-        const indice = leerLista().findIndex(contacto => contacto.id === id);
-        if (indice !== -1) {
-          window.localStorage.setItem("lista",JSON.stringify(leerLista().splice(indice, 1)));
-          actualizarLista();
-        }
+        const nuevaLista = leerLista().filter(contacto => contacto.id != id);
+        window.localStorage.setItem("lista",JSON.stringify(nuevaLista));
+        refrescarLista()
     }
 
     submitButton.addEventListener("click",()=>{
@@ -71,15 +83,28 @@ document.addEventListener("DOMContentLoaded", ()=>{
         let nuevoTelefono = cellInput.value
 
         agregarContacto(nuevoNombre, nuevoTelefono)
-
-        refrescarLista()
-
     })
 
     const refrescarLista = ()=> {
         document.getElementById("contact-list").innerHTML = ""
         leerLista().forEach(c => {
-            document.getElementById("contact-list").innerHTML += convertToHTMLElement(c.id,c.nombre,c.telefono)
+            let newContactElem = document.createElement("div")
+            newContactElem.innerHTML = convertToHTMLElement(c.id,c.nombre,c.telefono)
+            newContactElem = newContactElem.firstElementChild
+            //console.log(newContactElem.firstElementChild)
+            newContactElem.getElementsByClassName("delete-button")[0].addEventListener("click",()=>{
+                eliminarContacto(c.id)
+            })
+            newContactElem.getElementsByClassName("edit-menu")[0].addEventListener("click",()=>{
+                modalDOM = document.getElementById("myModal")
+                modalDOM.getElementsByTagName("h6")[0].innerHTML = `Ingresa nuevo numero para <strong>${c.nombre}</strong>:`
+                modalDOM.getElementsByTagName("input")[0].value = c.telefono
+                //console.log(modalDOM.getElementsByClassName("edit-button")[0])
+                modalDOM.getElementsByClassName("edit-button")[0].addEventListener("click", ()=>{
+                    actualizarContacto(c.id, modalDOM.getElementsByTagName("input")[0].value)
+                })
+            })
+            document.getElementById("contact-list").appendChild(newContactElem)
         });
     }
 
